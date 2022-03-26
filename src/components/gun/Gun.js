a054a681a648d5de0653132e05273e97e545907c
mamuntufayel <mamuntuffa@gmail.com>
2022-03-25 20:10:28 -0400

    onclick added with function

--- src/components/gun/Gun.js
+++ src/components/gun/Gun.js
@@ -4,8 +4,10 @@ import React from 'react';
 import './Gun.css'
 
 const Gun = (props) => {
-    console.log(props);
-    const { name, img, price, id } = props.gun
+    // console.log(props);
+    const { handleAddToCart } = props;
+    const { name, img, price, id } = props.gun;
+
     return (
         <div className='Each-gun'>
             <img src={img} alt="" />
@@ -14,7 +16,7 @@ const Gun = (props) => {
                 <h3>gun name:{name}</h3>
                 <h4>Price: ${price}</h4>
             </div>
-            <button className='btn-info'>
+            <button onClick={() => handleAddToCart(props.gun)} className='btn-info'>
                 <p>Add To Cart
                     <span>  <FontAwesomeIcon className='icon' icon={faShoppingCart} ></FontAwesomeIcon> </span>
                 </p>
--- src/components/guns/Firearms.js
+++ src/components/guns/Firearms.js
@@ -6,11 +6,17 @@ import './Firearms.css'
 const Firearms = () => {
     const url = "products.json"
     const [guns, setGuns] = useState([])
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(data => setGuns(data))
     }, [])
+
+    const handleAddToCart = (gun) => {
+        console.log(gun);
+    }
+
     return (
         <div>
             <h1 style={{ textAlign: 'center', fontSize: '60px', color: 'grey' }}>Fire Arms</h1>
@@ -22,11 +28,13 @@ const Firearms = () => {
                         guns.map(gun => <Gun
                             key={gun.id}
                             gun={gun}
+                            handleAddToCart={handleAddToCart}
                         ></Gun>)
                     }
                 </div>
                 <div className="firearm-cart-summary">
                     <h2 style={{ textAlign: 'center', fontSize: '40px', color: 'grey' }}>Summary</h2>
+                    <h4>Selected items:</h4>
                 </div>
             </div>
         </div>
